3a3cd01565d08d8881f19a52de1ed6b7f0a1782d
luzhihong <xiaopikaqiu@outlook.com>
2023-11-01 16:29:10 +0800

    getxdemo6_2

---
+++ android/app/src/main/kotlin/com/example/getxdemo07/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.getxdemo07
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  // final TextEditingController _editingController = TextEditingController();
+  final TextEditingController _editingController = TextEditingController();
+
+  MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Title'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Padding(
+            //   padding: const EdgeInsets.all(10),
+            //   child: TextField(
+            //     controller: _editingController,
+            //   ),
+            // ),
+            Padding(padding: const EdgeInsets.all(15),
+              child: TextField(
+                controller: _editingController,
+              ),
+            ),
+            const SizedBox(height: 20),
+            // ElevatedButton(
+            //     onPressed: () {
+            //       if (GetUtils.isEmail(_editingController.text)) {
+            //         Get.snackbar("正确", "恭喜你, 完全正确",
+            //             backgroundColor: Colors.greenAccent);
+            //       } else {
+            //         Get.snackbar("邮箱错误", "请输入正确的邮箱",
+            //             backgroundColor: Colors.pink);
+            //       }
+            //     },
+            //     child: const Text("验证邮箱")),
+            ElevatedButton(onPressed: () {
+              if (GetUtils.isNum(_editingController.text)){
+                Get.snackbar("正确", "是数字",backgroundColor: Colors.greenAccent);
+              }else{
+                Get.snackbar("错误", "请输入正确的数组", backgroundColor: Colors.pink);
+              }
+            }, child: const Text('是否为空')),
+            ElevatedButton(
+                onPressed: () {
+                  if (GetUtils.isPhoneNumber(_editingController.text) &&
+                      _editingController.text.length == 11) {
+                    Get.snackbar("正确", "恭喜你, 完全正确",
+                        backgroundColor: Colors.greenAccent);
+                  } else {
+                    Get.snackbar("手机号错误", "请输入正确的手机号",
+                        backgroundColor: Colors.pink);
+                  }
+                },
+                child: const Text("验证手机号")),
+          ],
+        ),
+      ),
+    );
+  }
+}
